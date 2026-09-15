f2cfda79e98dcf19bb47d1c5e689958ea0d12510
Ana Uzunova <amiagfdf@gmail.com>
2026-09-15 19:26:53 +0300

    edit of aquarium

--- simple calc/aquarium.py
+++ simple calc/aquarium.py
@@ -5,4 +5,3 @@ taken_space_procent=float(input())/100
 volume=length*width*height
 space_for_water=volume-taken_space_procent*volume
 print(space_for_water/1000,'L')
-#print('Volume in cm3:',volume)
